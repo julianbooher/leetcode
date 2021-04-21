fa8962f1d627c268dfbbe7c4a1426bd0cbffbdd2
julianbooher <julianbooher@hotmail.com>
2021-04-21 10:00:06 -0500

    changed up the logic to using counters and booleans, working solution

--- length-last-word.js
+++ length-last-word.js
@@ -13,19 +13,34 @@
 // Input: s = " "
 // Output: 0
 
+// var lengthOfLastWord = function(s) {
+//     for(let i = s.length -1; i >= 0; i--){
+//         if (s[i] === " " && s[i - 1] !== " "){
+//             return s.slice(i + 1, s.length).length;
+//         } else if (s[i] === " " && s.length > 1){
+//             return s.slice(0, i).length
+//         }
+//     }
+//     return 0;
+// };
+
 var lengthOfLastWord = function(s) {
+
+    let counter = 0;
+    let lastWord = false;
+
     for(let i = s.length -1; i >= 0; i--){
-        if (s[i] === " " && i !== s.length - 1){
-            return s.slice(i + 1, s.length).length;
-        } else if (s[i] === " " && s.length > 1){
-            return s.slice(0, i).length
+        if (s[i] !== ' '){
+            counter ++;
+            lastWord = true;
+        } else if (s[i] === ' ' && lastWord){
+            return counter;
         }
     }
-    return s.length;
+    return counter;
 };
-
 
 console.log('test 1, output should be 5:', lengthOfLastWord("Hello World") );
 console.log('test 2, output should be 0:', lengthOfLastWord(" ") )
 console.log('test 3, output should be 1:', lengthOfLastWord(" a") )
-console.log('test , output should be 1:', lengthOfLastWord("a ") )
+console.log('test 4, output should be 1:', lengthOfLastWord("a ") )
