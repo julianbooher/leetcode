48f177c7cbb0ccd5b9a3f7bc4b93520bb7ecbc33
julianbooher <julianbooher@hotmail.com>
2021-04-20 20:39:04 -0500

    added weirder test cases

--- length-last-word.js
+++ length-last-word.js
@@ -14,9 +14,16 @@
 // Output: 0
 
 var lengthOfLastWord = function(s) {
-    
+    for(let i = s.length -1; i >= 0; i--){
+        if (s[i] === " "){
+            return s.slice(i + 1, s.length).length;
+        }
+    }
+    return s.length;
 };
 
 
 console.log('test 1, output should be 5:', lengthOfLastWord("Hello World") );
 console.log('test 2, output should be 0:', lengthOfLastWord(" ") )
+console.log('test 3, output should be 1:', lengthOfLastWord(" a") )
+console.log('test 4, output should be 1:', lengthOfLastWord("a ") )
