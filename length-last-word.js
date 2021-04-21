fd993b9b2d0dfa5f169b54b116684d2435171f3b
julianbooher <julianbooher@hotmail.com>
2021-04-20 20:27:05 -0500

    initial setup

---
+++ length-last-word.js
@@ -0,0 +1,22 @@
+// Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Example 2:
+
+// Input: s = " "
+// Output: 0
+
+var lengthOfLastWord = function(s) {
+    
+};
+
+
+console.log('test 1, output should be 5:', lengthOfLastWord("Hello World") );
+console.log('test 2, output should be 0:', lengthOfLastWord(" ") )
