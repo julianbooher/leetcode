1f57fd46b64f08660da47d20efeceb9ac5bcf705
julianbooher <julianbooher@hotmail.com>
2021-04-20 20:45:24 -0500

    super hacky and test case 2 is broken again

--- length-last-word.js
+++ length-last-word.js
@@ -15,8 +15,10 @@
 
 var lengthOfLastWord = function(s) {
     for(let i = s.length -1; i >= 0; i--){
-        if (s[i] === " "){
+        if (s[i] === " " && i !== s.length - 1){
             return s.slice(i + 1, s.length).length;
+        } else if (s[i] === " " && s.length > 1){
+            return s.slice(0, i).length
         }
     }
     return s.length;
@@ -26,4 +28,4 @@ var lengthOfLastWord = function(s) {
 console.log('test 1, output should be 5:', lengthOfLastWord("Hello World") );
 console.log('test 2, output should be 0:', lengthOfLastWord(" ") )
 console.log('test 3, output should be 1:', lengthOfLastWord(" a") )
-console.log('test 4, output should be 1:', lengthOfLastWord("a ") )
+console.log('test , output should be 1:', lengthOfLastWord("a ") )
